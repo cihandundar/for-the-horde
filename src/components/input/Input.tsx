44298f7ced081ebeaa23254b7537804d756bb24c
cihandundar <94462152+cihandundar@users.noreply.github.com>
2025-05-30 23:15:33 +0300

    label fix

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -19,11 +19,40 @@ export default function Input({
                 onChange={onChange}
                 type={type}
                 disabled={disabled}
-                className='outline-none p-4 border-2 border-neutral-300 w-full rounded-md peer focus:border-neutral-900'
+                placeholder=" "
+                className="outline-none p-4 border-2 border-neutral-300 w-full rounded-md peer focus:border-neutral-900"
             />
-            <label className='capitalize absolute top-0 left-3 scale-75 peer-focus-within:scale-100 peer-focus-within:top-3 peer-focus-within:bg-white
-            peer-focus-within:px-2 px-0 bg-transparent transition-all duration-200 ease-in-out
-            '>
+            <label
+                className="
+                  capitalize
+                  absolute
+                  left-4
+                  top-4
+                  text-base
+                  pointer-events-none
+                  bg-white
+                  px-1
+                  transition-all
+                  duration-200
+                  ease-in-out
+                  peer-placeholder-shown:top-4
+                  peer-placeholder-shown:left-4
+                  peer-placeholder-shown:text-base
+                  peer-placeholder-shown:scale-100
+                  peer-focus:-top-2
+                  peer-focus:left-3
+                  peer-focus:text-xs
+                  peer-focus:scale-90
+                  peer-focus:bg-white
+                  peer-focus:px-1
+                  peer-[&:not(:placeholder-shown)]:-top-2
+                  peer-[&:not(:placeholder-shown)]:left-3
+                  peer-[&:not(:placeholder-shown)]:text-xs
+                  peer-[&:not(:placeholder-shown)]:scale-90
+                  peer-[&:not(:placeholder-shown)]:bg-white
+                  peer-[&:not(:placeholder-shown)]:px-1
+                "
+            >
                 {label}
             </label>
         </div>
